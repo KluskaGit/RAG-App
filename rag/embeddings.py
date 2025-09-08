89e5070b93f1ba99dc5798f6e4b8e8ea043cbf69
Kamil Kluska <kamil6kluska7@gmail.com>
2025-09-08 22:54:47 +0200

    querry and passage prefix for e5 models only

--- rag/embeddings.py
+++ rag/embeddings.py
@@ -2,12 +2,26 @@ from sentence_transformers import SentenceTransformer
 from langchain_core.documents import Document
 
 class Embedder:
+    '''
+    Embedder using SentenceTransformer model.
+    Default model is 'intfloat/multilingual-e5-base' which supports multiple languages.'
+    For e5 models, prepend "passage: " to passages and "query: " to queries.
+
+    Args:
+        model: str - The name of the SentenceTransformer model to use.
+    '''
     def __init__(self, model: str='intfloat/multilingual-e5-base'):
-        self.model = SentenceTransformer(model)
+        self.model = model
+        self.transformer = SentenceTransformer(model)
+
 
     def embed_texts(self, chunks: list[Document]):
-        texts = [f"passage: {doc.page_content}" for doc in chunks]
-        return self.model.encode(texts)
+        texts = [doc.page_content for doc in chunks]
+        if 'e5' in self.model.lower():
+            texts = [f"passage: {doc.page_content}" for doc in chunks]
+        return self.transformer.encode(texts)
 
     def embed_query(self, query: str):
-        return self.model.encode([f"query: {query}"])
+        if 'e5' in self.model.lower():
+            query = f"query: {query}"
+        return self.transformer.encode([query])
