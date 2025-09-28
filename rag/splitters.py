79d54612b80cfbc2b4da547edd6fa82225c38c73
KluskaGit <kamil6kluska7@gmail.com>
2025-09-28 15:19:36 +0200

    own text splitter with tokenizer

--- rag/splitters.py
+++ rag/splitters.py
@@ -1,11 +1,27 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_core.documents import Document
+import tiktoken
 
-def chunk_documents(documents: list[Document]) -> list[Document]:
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500, 
-        chunk_overlap=50,
-        length_function=len,
-        is_separator_regex=False,
-    )
-    return text_splitter.split_documents(documents)
+class TokenTextSplitter:
+    def __init__(
+            self, chunk_size: int=300,
+            overlap: int=50,
+            tokenizer: str='cl100k_base',
+            ):
+        
+        self.chunk_size = chunk_size
+        self.overlap = overlap
+        self.enc= tiktoken.get_encoding(tokenizer)
+
+    def split_text(self, text: str) -> list[str]:
+        tokens = self.enc.encode(text)
+
+        chunks: list[str] = []
+        start = 0
+
+        while start < len(tokens):
+            end = start + self.chunk_size
+            chunk = self.enc.decode(tokens[start:end])
+            chunks.append(chunk)
+            start = end - self.overlap
+
+        return chunks
+
