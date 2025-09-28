2935b74511296813141c69391509a8f1998f8f3c
KluskaGit <kamil6kluska7@gmail.com>
2025-09-28 15:18:14 +0200

    loader object

--- rag/loaders.py
+++ rag/loaders.py
@@ -1,25 +1,19 @@
-from bs4.filter import SoupStrainer
-from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, WebBaseLoader
-from langchain_core.documents import Document
-from typing import List
+from pypdf import PdfReader # Alternatively use PyMuPDF
+from rag.schemas.document import Document
 
+class FileLoader:
+    
+    def load_pdf(self, path: str) -> list[Document]:
+        reader = PdfReader(path)
+        documents: list[Document] = []
+        for page in reader.pages:
+            text = page.extract_text()
+            documents.append(Document(text=text))
 
-
-def load_pdf(path: str) -> List[Document]:
-    loader = PyPDFLoader(file_path=path)
-    return loader.load()
-
-# def load_sites(links: List[str]) -> List[Document]:
-#     bs4_strainer = SoupStrainer(class_=("post-title", "post-header", "post-content"))
-#     loader = WebBaseLoader(
-#         web_paths=links,
-#         bs_kwargs={"parse_only": bs4_strainer},
-#     )
-#     return loader.load()
-
-
-def load_file(path: str) -> List[Document]:
-    if path.endswith(".pdf"):
-        return load_pdf(path)
-    else:
-        raise ValueError(f"Unsupported file type: {path}")
+        return documents
+    
+    def load_file(self, path: str) -> list[Document]:
+        if path.endswith(".pdf"):
+            return self.load_pdf(path)
+        else:
+            raise ValueError(f"Unsupported file type: {path}")
