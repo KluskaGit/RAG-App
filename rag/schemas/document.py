e425924d3b0691c52843d5cd43475a6bd39ba2d0
KluskaGit <kamil6kluska7@gmail.com>
2025-10-08 18:27:35 +0200

    Added types matching Metadata object in ChromaDB

--- rag/schemas/document.py
+++ rag/schemas/document.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel, Field
-from typing import Any
 
 
 class Document(BaseModel):
     text: str = Field(..., description="The content of the document")
-    metadata: dict[str, Any] | None = Field(default=None, description="Metadata associated with the document like source, author, etc.")
+    metadata: dict[str, str | int | float | bool] | None = Field(default=None, description="Metadata associated with the document like source, author, etc.")
