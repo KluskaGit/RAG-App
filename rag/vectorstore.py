ada25877573788ecb117e44cbcadf42f75153d3c
KluskaGit <kamil6kluska7@gmail.com>
2025-10-11 23:28:53 +0200

    similarity search and embedding

--- rag/vectorstore.py
+++ rag/vectorstore.py
@@ -1,22 +1,24 @@
 import chromadb
 import uuid
-from chromadb.api.types import Embeddings, Metadata
+from chromadb.api.types import EmbeddingFunction, Metadata, QueryResult
+from chromadb.utils.embedding_functions import DefaultEmbeddingFunction
 from rag.schemas.document import Document
 
 class VectorStore:
 
     def __init__(
             self,
-            persist_directory: str
+            collection_name: str,
+            persist_directory: str,
+            embedding: EmbeddingFunction = DefaultEmbeddingFunction(),
         ):
 
         self.client = chromadb.PersistentClient(path=persist_directory)
-        self.collection = self.client.get_or_create_collection(name="documents")
+        self.collection = self.client.get_or_create_collection(name=collection_name, embedding_function=embedding)
 
     def save_texts(
             self,
             texts: list[str],
-            #embeddings: Embeddings,
             metadatas: list[Metadata] | None = None,
             
         ):
@@ -24,7 +26,6 @@ class VectorStore:
             ids=[str(uuid.uuid4()) for _ in texts],
             documents=texts,
             metadatas=metadatas,
-            #embeddings=embeddings
         )
 
     def save_documents(self, documents: list[Document]):
@@ -35,3 +36,14 @@ class VectorStore:
             metadatas.append(doc.metadata if doc.metadata else {})
 
         self.save_texts(texts, metadatas)
+
+    def similarity_search(
+            self,
+            query: str,
+            k: int = 3
+        ) -> QueryResult:
+
+        return self.collection.query(
+            query_texts=query,
+            n_results=k
+        )
