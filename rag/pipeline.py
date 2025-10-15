8181788ee5f01429acccfe6083fe0e2b5d86bb44
KluskaGit <kamil6kluska7@gmail.com>
2025-10-15 12:13:26 +0200

    Pipeline for RAG

--- rag/pipeline.py
+++ rag/pipeline.py
@@ -1,30 +1,41 @@
-# from rag.loaders import load_file
-# from rag.splitters import chunk_documents
-# #from langchain_community.llms import HuggingFaceHub
-# from rag.embeddings import save_to_vector_store, load_chroma, generate
-
-# def pipe(query: str):
-#     file = load_file('data/Zalacznik-Nr-5.pdf')
-#     all_splits = chunk_documents(file)
-
-#     vectore_store = load_chroma('vectordb')#save_to_vector_store(all_splits, path="vectordb")
-#     results = vectore_store.similarity_search(query, k=3)
-
-#     prompt = f'''
-#     You are a helpful chatbot.
-#     Use only the following pieces of context to answer the question. Don't make up any new information:
-#     {'\n'.join([doc.page_content for doc in results])}
-#     '''
-#     print(prompt)
-#     return generate(query, prompt).message.content
+import os
+from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 
 from rag.loaders import FileLoader
 from rag.splitters import TokenTextSplitter
 from rag.vectorstore import VectorStore
 
-loader = FileLoader()
-doc = loader.load_file('data/Zalacznik-Nr-5.pdf')
-splitter = TokenTextSplitter(chunk_size=500, overlap=50)
-chunks = splitter.split_documents(doc)
-vectoreStore = VectorStore('vectordb')
-vectoreStore.save_documents(chunks)
+class Pipeline:
+    def __init__(
+            self,
+            embedding_model_name: str = "sentence-transformers/all-mpnet-base-v2"
+        ):
+        self.loader = FileLoader()
+        self.splitter = TokenTextSplitter(chunk_size=500, overlap=150)
+        self.huggingface_ef = SentenceTransformerEmbeddingFunction(
+            model_name=embedding_model_name
+        )
+        self.vectoreStore = VectorStore('documents', 'vectordb', embedding=self.huggingface_ef)
+
+    def save_data(self, folder_path: str) -> None:
+        file_names: list[str] = os.listdir(folder_path)
+
+        for file in file_names:
+            file_path = os.path.join(folder_path, file)
+            documents = self.loader.load_file(file_path)
+            chunks = self.splitter.split_documents(documents)
+            self.vectoreStore.save_documents(chunks)
+
+    def generate_response(self, query: str) -> tuple[str, list]:
+        result = self.vectoreStore.similarity_search(query=query)
+        if context:=result.get('documents', None):
+            context = '\n'.join(context[0])
+        else:
+            context = "Brak wyników"
+
+        if metadata:= result.get('metadatas', None):
+            metadata = metadata[0]
+        else:
+            metadata = []
+        return context, metadata
+        
