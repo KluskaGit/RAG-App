18c9c47a04fe2cdd8a66d23faf65edaf67b46f5d
Kamil Kluska <kamil6kluska7@gmail.com>
2025-10-17 19:02:05 +0200

    appconfig file

--- rag/pipeline.py
+++ rag/pipeline.py
@@ -1,4 +1,8 @@
 import os
+import yaml
+from openai import OpenAI
+from pathlib import Path
+from dotenv import load_dotenv
 from chromadb.utils.embedding_functions import OllamaEmbeddingFunction
 
 from rag.loaders import FileLoader
@@ -6,19 +10,25 @@ from rag.splitters import TokenTextSplitter
 from rag.vectorstores.chroma_local import ChromaLocal
 
 class Pipeline:
-    def __init__(
-            self,
-            embedding_model_name: str = "jeffh/intfloat-multilingual-e5-large-instruct:f32"
-        ):
+    def __init__(self):
+        load_dotenv()
         self.loader = FileLoader()
         self.splitter = TokenTextSplitter(chunk_size=500, overlap=150)
 
-        embedding = OllamaEmbeddingFunction(model_name=embedding_model_name)
-        self.vectoreStore = ChromaLocal(
-            host = 'localhost',
-            port = 8000,
-            collection_name = 'documents',
-            embedding=embedding)
+        with open(Path('appconfig.yaml'), 'r') as cfg:
+            self.config = yaml.safe_load(cfg)
+
+        embedding = OllamaEmbeddingFunction(
+            url=self.config['retriever']['ollama_base_url'],
+            model_name=self.config['retriever']['model']
+        )
+
+        self.vectorStore = ChromaLocal(
+            host = self.config['vectorstore']['host'],
+            port = self.config['vectorstore']['port'],
+            collection_name = self.config['vectorstore']['collection_name'],
+            embedding=embedding
+        )
 
     def save_data(self, folder_path: str) -> None:
         file_names: list[str] = os.listdir(folder_path)
@@ -27,10 +37,10 @@ class Pipeline:
             file_path = os.path.join(folder_path, file)
             documents = self.loader.load_file(file_path)
             chunks = self.splitter.split_documents(documents)
-            self.vectoreStore.save_documents(chunks)
+            self.vectorStore.save_documents(chunks)
 
     def generate_context(self, query: str) -> tuple[str, list]:
-        result = self.vectoreStore.similarity_search(query=query)
+        result = self.vectorStore.similarity_search(query=query)
         if context:=result.get('documents', None):
             context = '\n'.join(context[0])
         else:
@@ -41,3 +51,27 @@ class Pipeline:
         else:
             metadata = []
         return context, metadata
+    
+    def generate_response(self, prompt: str) -> tuple[str | None, list]:
+        context, metadata = self.generate_context(query=prompt)
+        
+        system_prompt = f"""
+            You are a helpful chatbot.
+            Use only the following pieces of context to answer the question. Don't make up any new information:
+            {context}
+        """
+        client = OpenAI(
+            base_url=self.config['LLM']['base_url'],
+            api_key=os.environ[self.config['LLM']['api_key']],
+        )
+        
+        response = client.chat.completions.create(
+            model=self.config['LLM']['model'],
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": f"Answer the question based on the context: {prompt}"},
+            ]
+        )
+        message = response.choices[0].message.content
+
+        return message, metadata
--- ui/chat.py
+++ ui/chat.py
@@ -1,15 +1,15 @@
 import os
 import streamlit as st
-import ollama
-from openai import OpenAI
-from dotenv import load_dotenv
 
 from rag.pipeline import Pipeline
 
 class Chat():
     def __init__(self):
-        load_dotenv()
         st.title("RAG App")
+
+        # Initialize Pipeline
+        self.pipe = Pipeline()
+
         # Initialize chat history
         if "messages" not in st.session_state:
             st.session_state.messages = []
@@ -32,31 +32,7 @@ class Chat():
 
             # Display assistant response in chat message container
             with st.chat_message("assistant"):
-                pipe = Pipeline(
-                    embedding_model_name="jeffh/intfloat-multilingual-e5-large-instruct:f32",
-                    #api_key=os.environ["HF_TOKEN"]
-                )
-                pipe.save_data('data')
-                context, metadata = pipe.generate_context(query=prompt)
-                print(context)
-                system_prompt = f"""
-                    You are a helpful chatbot.
-                    Use only the following pieces of context to answer the question. Don't make up any new information:
-                    {context}
-                """
-                client = OpenAI(
-                    base_url="https://router.huggingface.co/v1",
-                    api_key=os.environ["HF_TOKEN"],
-                )
-                
-                response = client.chat.completions.create(
-                    model="openai/gpt-oss-20b:nebius",
-                    messages=[
-                        {"role": "system", "content": system_prompt},
-                        {"role": "user", "content": f"Answer the question based on the context: {prompt}"},
-                    ]
-                )
-                message = response.choices[0].message.content
+                message, metadata = self.pipe.generate_response(prompt)
                 st.write(message, metadata)
             # Add assistant response to chat history
             st.session_state.messages.append({"role": "assistant", "content": message})
